4fb3ad48b1331724164d2cc9f202017a114024a1
temph2024 <164739158+temph2024@users.noreply.github.com>
2024-03-24 16:16:53 +0530

    Update getviews.py

--- getviews.py
+++ getviews.py
@@ -143,8 +143,8 @@ def tui():
 
 clear_output()
 print(logo)
-channel = "the_hacking_zone"
-post = "1121"
+channel = os.environ.get("channel")
+post = os.environ.get("post")
 
 try:
     search('<span class="tgme_widget_message_views">([^<]+)', requests.get(f'https://t.me/{channel}/{post}',
