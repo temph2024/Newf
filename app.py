30d5ddf305126cb87532625379a485ef23d779e4
temph2024 <164739158+temph2024@users.noreply.github.com>
2024-05-25 19:51:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+import subprocess
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'This is Joneswarrior. Bitch bakchodi nhi samajh me aaya kya. Sabki maa ki chut.'
 
-
 if __name__ == "__main__":
+    # Run the additional scripts
+    subprocess.Popen(['python3', 'scrape.py'])
+    time.sleep(40)
+    subprocess.Popen(['python3', 'getviews.py'])
+    
+    # Start the Flask app
     app.run()
